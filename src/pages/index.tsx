8b444353ff51f2a5218b71d85f3a1596e239ae08
조보화 <jobong212@gmail.com>
2025-06-21 16:21:03 +0900

    build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,16 +24,6 @@ export const metadata: Metadata = {
   }
 }
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
     <>
