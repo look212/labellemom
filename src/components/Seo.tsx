3dcac3c5a3bc321d121af11065fe202824dab948
조보화 <jobong212@gmail.com>
2025-07-02 22:14:59 +0900

    Seo 추가 수정

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -20,10 +20,10 @@ export default function Seo({
             <meta name="description" content={description ? description : env.siteDescription} />
             <meta name="keywords" content={env.siteKeywords} />
             <link rel="icon" href="/favicon.ico" />
-            <meta property="og:title" content={env.siteName} />
-            <meta property="og:description" content={env.siteDescription} />
+            <meta property="og:title" content={title ? title : env.siteName} />
+            <meta property="og:description" content={description ? description : env.siteDescription} />
             <meta property="og:url" content={url ? url : env.siteUrl} />
-            <meta property="og:site_name" content={env.siteName} />
+            <meta property="og:site_name" content={title ? title : env.siteName} />
             <meta property="og:image" content={image ? image : env.siteImage} />
             <meta property="og:locale" content={env.siteLocale} />
             <meta property="og:type" content={env.siteType} />
